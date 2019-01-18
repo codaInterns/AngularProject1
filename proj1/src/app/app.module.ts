2d2d52edd1c3ad0f64b8923bdd47dfd9d08750c7
ultra_instinct <anandkarthikeyan1998@gmail.com>
2019-01-18 14:53:08 +0530

    nav-feature added

--- proj1/src/app/app.module.ts
+++ proj1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { NavFeatureComponent } from './components/nav-feature/nav-feature.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavFeatureComponent
   ],
   imports: [
     BrowserModule,

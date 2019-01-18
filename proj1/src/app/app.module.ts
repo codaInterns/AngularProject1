063659f74f80be8ef58fd86dfcf3a4c92f7f83f8
nileshkumarb <nileshb3205@gmail.com>
2019-01-18 16:40:58 +0530

    register commit

--- proj1/src/app/app.module.ts
+++ proj1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

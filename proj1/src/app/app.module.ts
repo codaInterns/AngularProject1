4c276d7093469683028798dc869f4cbb05a87524
97balakrishnan <97balakrishnan@gmail.com>
2019-01-18 17:47:52 +0530

    flight search results component initial commit

--- proj1/src/app/app.module.ts
+++ proj1/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
@@ -9,18 +9,21 @@ import { FlightSearchComponent } from './components/flight-search/flight-search.
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { FlightSearchResultComponent } from './components/flight-search-result/flight-search-result.component';
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    FlightSearchComponent
+    FlightSearchComponent,
+    FlightSearchResultComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

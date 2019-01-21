1ff1238a962868ef6c980db713d2a00ccc5c55e2
ultra_instinct <anandkarthikeyan1998@gmail.com>
2019-01-21 11:31:47 +0530

    changes in module

--- proj1/src/app/app.module.ts
+++ proj1/src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes} from '@angular/router';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { NavFeatureComponent } from './components/nav-feature/nav-feature.component';
 import { FlightsComponent } from './views/flights/flights.component';
+
+const appRoutes: Routes = [
+  {
+     path: '',
+     component: DashboardComponent
+  },
+  {
+     path: 'app-fligts',
+     component: FlightsComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
@@ -18,7 +33,8 @@ FlightsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

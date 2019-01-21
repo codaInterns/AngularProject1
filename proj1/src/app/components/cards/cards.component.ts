98c1a416d9641f59be30a29b7a87625830b5fb89
ultra_instinct <anandkarthikeyan1998@gmail.com>
2019-01-21 10:20:47 +0530

    changes

--- proj1/src/app/components/cards/cards.component.ts
+++ proj1/src/app/components/cards/cards.component.ts
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-=======
 import { Component, OnInit, Input } from '@angular/core';
->>>>>>> 5ea25729a6018fcbcacded6412bfafeaf952b09e
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
+
 export class CardsComponent implements OnInit {
 
-<<<<<<< HEAD
-=======
   @Input()
   city: string;
 
->>>>>>> 5ea25729a6018fcbcacded6412bfafeaf952b09e
   constructor() { }
 
   ngOnInit() {

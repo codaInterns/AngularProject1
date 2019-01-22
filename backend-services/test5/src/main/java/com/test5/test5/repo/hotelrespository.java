463e7f1af46d784894c712d1a440c3c0c0525437
gowthamkathier <ggowthamkathier@gmail.com>
2019-01-22 16:58:00 +0530

    Revert "Revert "hotel serivces added""
    This reverts commit 5e66de5f18b7f4b3e2905005646c94bc699b1d5e.

---
+++ backend-services/test5/src/main/java/com/test5/test5/repo/hotelrespository.java
@@ -0,0 +1,11 @@
+package com.test5.test5.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.test5.test5.models.hotels;
+
+public interface hotelrespository extends JpaRepository<hotels, Integer>{
+	
+	
+
+}

9605904fc3a52a189a67b8d606c91e4a824d4edf
kaushiks15103 <kaushiksivaram11@gmail.com>
2019-01-22 11:21:12 +0530

    Changed SQL Column Names

--- proj1/userdata.sql
+++ proj1/userdata.sql
@@ -1,4 +1,6 @@
-create table flights(flight_id int(20),name varchar(50),source varchar(50),destination varchar(50),departure varchar(50),price int);
+drop table flights;
+
+create table flights(flights_id int(20),name varchar(50),source varchar(50),destination varchar(50),departure varchar(50),price int);
 
 
 insert into flights values(1,'indigo','chennai','delhi','04:00:00' , 1200);
@@ -7,8 +9,10 @@ insert into flights values(1,'air india','chennai','Mumbai','04:30:00',2300);
 
 insert into flights values(1,'jet airways','delhi','Toronto','03:00:00',12000);
 
-create users (
-    id int(11) primary key,
+drop table users;
+create table users (
+
+    id int(11) primary key  AUTO_INCREMENT,
     email varchar(255) not null,
     password varchar(255) not null
 );

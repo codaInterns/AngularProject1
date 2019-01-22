b5776767160afacc92b200d8b248fb8c5d206ea9
kaushiks15103 <kaushiksivaram11@gmail.com>
2019-01-22 11:56:42 +0530

    Formatting Changes

--- proj1/src/app/app.module.ts
+++ proj1/src/app/app.module.ts
@@ -37,12 +37,12 @@ const appRoutes: Routes = [
     component: FlightsComponent
   },
   {
-    path : "login",
-    component : UserloginComponent
+    path: "login",
+    component: UserloginComponent
   },
   {
-    path : "register",
-    component : RegisterComponent
+    path: "register",
+    component: RegisterComponent
   }
 ];
 
@@ -78,4 +78,4 @@ const appRoutes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }

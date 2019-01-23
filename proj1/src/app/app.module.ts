7001471d68eae2ee93bddaab8a5575a1c94db66b
WhiteWolfie <n.r.sarabesh@gmail.com>
2019-01-23 10:49:33 +0530

    resolved

--- proj1/src/app/app.module.ts
+++ proj1/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
 import {ReactiveFormsModule} from '@angular/forms';
-
+import {Routes,RouterModule} from '@angular/router';
 import { UserloginComponent } from "./components/userlogin/userlogin.component";
 import { HttpClientModule } from "@angular/common/http";
 

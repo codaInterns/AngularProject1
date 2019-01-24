49812b9f20241cc149e47aeaff635d1f879b4b39
ultra_instinct <anandkarthikeyan1998@gmail.com>
2019-01-24 16:00:20 +0530

    booked component

---
+++ proj1/src/app/components/booked/booked.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { BookServiceService } from 'src/app/services/book-service.service';
+
+@Component({
+  selector: 'app-booked',
+  templateUrl: './booked.component.html',
+  styleUrls: ['./booked.component.css']
+})
+export class BookedComponent implements OnInit {
+
+  isBooked:boolean;
+  constructor(private myservice:BookServiceService) { }
+
+  ngOnInit() {
+    this.isBooked=this.myservice.isBooked;
+  }
+
+}

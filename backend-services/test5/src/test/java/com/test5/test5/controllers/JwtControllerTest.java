a9de5db79e5d9545019e71bf8e37d5cd0df013d5
97balakrishnan <97balakrishnan@gmail.com>
2019-01-25 17:16:53 +0530

    JwtController test validation

---
+++ backend-services/test5/src/test/java/com/test5/test5/controllers/JwtControllerTest.java
@@ -0,0 +1,63 @@
+package com.test5.test5.controllers;
+
+import static org.hamcrest.Matchers.containsString;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.trace.http.HttpTrace.Response;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.test5.test5.models.FlightInput;
+import com.test5.test5.models.options;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JwtControllerTest {
+	
+	 @Autowired
+	 private MockMvc mockMvc;
+	 @Autowired 
+	 private ObjectMapper mapper;
+	 
+	
+	 @Test
+	 public void testJwtAuthController() throws Exception{
+		 options mockInput=new options();
+		 mockInput.setEmail("aa@aa.com");
+		 mockInput.setPassword("Kaushik@1");
+		
+		 String json=mapper.writeValueAsString(mockInput);
+		 
+		
+		 
+		 MvcResult mvcResult = mockMvc.perform(post("/api/jwt/")
+				 .content(json).contentType(MediaType.APPLICATION_JSON)
+				 .accept(MediaType.APPLICATION_JSON))
+		 		 .andExpect(status().isOk()).andReturn();
+ 		 String content = mvcResult.getResponse().getContentAsString();
+ 		 System.out.println(content);
+ 	
+	 }
+	
+}

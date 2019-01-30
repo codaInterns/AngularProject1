a67d69769d51cec949dbdfebdefc863308275dc6
Shivani_Donthi <shivanidonthi97@gmail.com>
2019-01-30 18:38:15 +0530

    Refractoring

---
+++ proj1/src/app/email.service_BASE_6716.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  constructor(private http:HttpClient) {
+
+   }
+
+   public getUser(email:String,password:String){
+    var options = {
+      email : email,
+      password : password
+        
+    };
+     return this.http.post("http://localhost:8080/api/jwt/",options,{responseType: 'text'});
+
+   }
+}

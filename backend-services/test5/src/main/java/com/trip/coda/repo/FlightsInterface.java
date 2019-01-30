27185218f94154d8573a37b118c4b1c0e308b4dc
Shivani_Donthi <shivanidonthi97@gmail.com>
2019-01-30 15:07:53 +0530

    removed test5

--- backend-services/test5/src/main/java/com/trip/coda/repo/FlightsInterface.java
+++
@@ -1,9 +0,0 @@
-package com.trip.coda.repo;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.trip.coda.models.Flight;
-
-public interface FlightsInterface extends JpaRepository<Flight, Long> {
-
-}

94bae77563bb8444152391216f8b9f97fd17b50a
WhiteWolfie <n.r.sarabesh@gmail.com>
2019-01-31 11:40:01 +0530

    Refacotored BookingController

--- backend-services/CodaTrips/src/main/java/com/trip/coda/controllers/BookingController.java
+++ backend-services/CodaTrips/src/main/java/com/trip/coda/controllers/BookingController.java
@@ -1,5 +1,7 @@
 package com.trip.coda.controllers;
 
+import com.trip.coda.models.BookingInput;
+import com.trip.coda.services.BookingService;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.trip.coda.models.BookingInput;
-import com.trip.coda.services.BookingService;
-
-
-
-
-
 @RestController
 @RequestMapping("/JwtAuth")
 @CrossOrigin("http://localhost:4200")
-public class BookingController {
-	@Autowired
-	BookingService bookingService; 
-
-	@PostMapping(path = "/book/" , consumes = "application/json" , produces = "application/json")
-	public ResponseEntity<Boolean> postBooking(@RequestBody BookingInput opt,final HttpServletRequest request) {
+public final class BookingController {
+  @Autowired
+  private BookingService bookingService;
+  @PostMapping(path = "/book/", consumes = "application/json", produces = "application/json")
+      public ResponseEntity<Boolean> postBooking(@RequestBody final BookingInput opt, final HttpServletRequest request) {
 		return bookingService.postUser(opt, request);
 	}
 }

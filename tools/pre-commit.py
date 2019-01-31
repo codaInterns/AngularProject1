2dd49df0376999a25dc7f264745d4bef96fd0278
kaushiksivaram11 <kaushiksivaram11@gmail.com>
2019-01-31 13:31:44 +0530

    Updated pre-commit.py

--- tools/pre-commit.py
+++ tools/pre-commit.py
@@ -3,8 +3,7 @@ import os
 import subprocess
 import sys
 from glob import glob
-def fun():
-	sys.stderr.write("Hiiiii")	
+def fun():	
 	commitids = os.popen('git log --format="%H" -n 2').read()
 	commitids = commitids.splitlines()
 	diff = os.popen('git diff --name-only ' + commitids[0] + ' ' + commitids[1]).read()
@@ -19,17 +18,22 @@ def fun():
 			if len(cmd) != 0:
 				isLintingProper = 0
 				sys.stderr.write(i)
-				sys.stderr.write(cmd)
+				sys.stderr.write(str(cmd))
 		elif os.path.splitext(base)[1] == '.java':
 			# print(basepath+i)
 			cmd = os.popen('java -jar ' + basepath+'tools\\java-linting-tools\\checkstyle-8.17-all.jar -c '+basepath + 'tools\\java-linting-tools\\google_checks.xml ' + basepath+i).read()
+			errors = cmd
 			cmd = cmd.splitlines()
-			cmd.remove('Starting audit...')
-			cmd.remove('Audit done.')
+			if cmd.count('Starting audit...') != 0:
+				cmd.remove('Starting audit...')
+			if cmd.count('Audit done.') != 0:
+				cmd.remove('Audit done.')
+			if cmd.count('Files to process must be specified, found 0.') != 0:
+				cmd.remove('Files to process must be specified, found 0.')	
 			if len(cmd) != 0:
 				isLintingProper = 0
 				sys.stderr.write(basepath+i)
-				sys.stderr.write(cmd)	
+				sys.stderr.write(str(errors))	
 
 
 	return isLintingProper
